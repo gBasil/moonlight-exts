1ab82edacd0d938c6d05640f1abdf53efa9d7fb8
Basil <basil@basil.cafe>
2025-01-05 06:52:45 +0000

    feat(dedupeMessages): init

---
+++ src/dedupeMessages/index.tsx
@@ -0,0 +1,25 @@
+import { ExtensionWebExports } from '@moonlight-mod/types';
+
+export const patches: ExtensionWebExports['patches'] = [
+	// NOTE: This does not work for messages with attachments
+	{
+		find: '_sendMessage',
+
+		replace: {
+			match: /async sendMessage\((.),(.)\){/,
+			replacement: (orig, channelId, messageData) => {
+				return `${orig}if(await require('dedupeMessages_handler').handle(${channelId},${messageData}))return;`;
+			}
+		}
+	}
+];
+
+export const webpackModules: ExtensionWebExports['webpackModules'] = {
+	handler: {
+		dependencies: [
+			{ ext: 'spacepack', id: 'spacepack' },
+			{ id: 'discord/utils/HTTPUtils' },
+			{ id: 'react' }
+		]
+	}
+};
---
+++ src/dedupeMessages/webpackModules/handler.tsx
@@ -0,0 +1,130 @@
+import { HTTP } from '@moonlight-mod/wp/discord/utils/HTTPUtils';
+import spacepack from '@moonlight-mod/wp/spacepack_spacepack';
+import React from '@moonlight-mod/wp/react';
+
+// Discord's URL RegEx
+const URL_REGEX = /https?:\/\/[^\s<]+[^<.,:;"')\]\s]/gm;
+
+type MessageData = {
+	content: string;
+	tts: boolean;
+	invalidEmojis: unknown[];
+	validNonShortcutEmojis: unknown[];
+};
+
+type SearchResponse = Awaited<ReturnType<typeof HTTP.get>> & {
+	body: {
+		messages: [{
+			content: string;
+		}][];
+		total_results: number;
+	};
+};
+
+interface Dispatcher {
+	dispatchToLastSubscribed(key: string, n: unknown): Dispatcher;
+}
+
+// There are more options here, but these are just the ones I use
+type AlertOptions = Partial<{
+	title: string;
+	body: string | React.JSX.Element;
+	cancelText: string;
+	confirmText: string;
+	/** Function that gets called when you pres the cancel button. */
+	onCancel: () => void;
+	onConfirm: () => void;
+	/** Function that gets called when you press escape. */
+	onCloseCallback: () => void;
+}>;
+interface Alerts {
+	show(options: AlertOptions): Promise<boolean>;
+}
+
+const Dispatcher = spacepack.findObjectFromKey(
+	spacepack.findByCode('_checkSavedDispatches')[0].exports,
+	'_checkSavedDispatches'
+) as Dispatcher;
+
+const Alerts = spacepack.findByCode('close(){},')[0].exports.Z as Alerts;
+
+const logger = moonlight.getLogger('dedupeMessages/handler');
+
+function linksInStr(str: string): string[] {
+	const links = Array.from(str.matchAll(URL_REGEX)).map(([url]) => url);
+	const unique = links.filter((url, i, arr) =>
+		!arr.slice(0, i).some(e => e === url)
+	);
+
+	return unique;
+}
+
+function confirmPrompt(options: AlertOptions): Promise<boolean> {
+	return new Promise(resolve => {
+		Alerts.show({
+			onCancel() { resolve(false); },
+			onConfirm() { resolve(true); },
+			onCloseCallback() {
+				setTimeout(() => resolve(false), 100);
+			},
+			cancelText: 'Cancel',
+			...options
+		});
+	})
+}
+
+// Returns a boolean indicating if message sending should abort
+export async function handle(channelId: number, data: MessageData): Promise<boolean> {
+	const channels = moonlight.getConfigOption<string[]>('dedupeMessages', 'channels') ?? [];
+	if (!channels.includes(channelId.toString())) return false;
+
+	const domains = moonlight.getConfigOption<string[]>('dedupeMessages', 'domains') ?? [];
+	const urls = linksInStr(data.content);
+	const duplicates = [];
+
+	for (const url of urls) {
+		const { host } = new URL(url);
+
+		if (!domains.includes(host)) continue;
+
+		try {
+			const data = (await HTTP.get({
+				url: `/channels/${channelId}/messages/search`,
+				query: { content: url }
+			})) as SearchResponse;
+
+			const hasBeenSent = data.body.messages.some(([msg]) => linksInStr(msg.content).includes(url));
+
+			if (hasBeenSent) duplicates.push(url);
+		} catch (e) {
+			logger.error('Failed to search URL', url, e);
+		}
+	}
+
+	if (duplicates.length > 0) {
+		const send = await confirmPrompt({
+			title: 'Send Message?',
+			confirmText: 'Send',
+			body: <>
+				<p>The following URLs have already been sent:</p>
+
+				<ul>
+					{duplicates.map(url => <li>
+						<a href={url} target='_blank'>{url}</a>
+					</li>)}
+				</ul>
+			</>
+		});
+
+		if (!send) {
+			Dispatcher.dispatchToLastSubscribed(
+				'INSERT_TEXT',
+				{ rawText: data.content }
+			);
+
+			return true;
+		}
+	}
+
+	return false;
+}
