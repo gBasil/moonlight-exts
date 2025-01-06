d9a2425151c22027fc098404a5a45bd33c0da3d4
Basil <basil@basil.cafe>
2025-01-06 18:21:35 +0000

    fix(replyChain): fix patch warning

--- src/replyChain/index.tsx
+++ src/replyChain/index.tsx
@@ -1,6 +1,5 @@
 import { ExtensionWebExports } from '@moonlight-mod/types';
 
-let fnArg: string;
 export const patches: ExtensionWebExports['patches'] = [
 	// Patch the function that gets called when you click a message reply to jump to the message
 	{
@@ -8,21 +7,9 @@ export const patches: ExtensionWebExports['patches'] = [
 
 		replace: [
 			{
-				match: /\.memo\(function\((.)\)/,
-				replacement: (orig, arg) => {
-					fnArg = arg;
-					// As of writing, moonlight shows a "Patch replacement failed" warning
-					// in the console if a patch is deemed to have not changed any code, even
-					// if it matched. Hopefully this will be fixed in future versions.
-					// (I know I can put an empty comment here to suppress the warning, but
-					// I'd rather wait until it's properly fixed)
-					return orig;
-				}
-			},
-			{
-				match: /onClickReply:(.),/,
-				replacement: (_, callback) =>
-					`onClickReply:(()=>{require('replyChain_handler').handle(${fnArg});${callback}()}),`
+				match: /\.memo\(function\((.)\)(.+?)onClickReply:(.),/,
+				replacement: (_, fnArg, orig, callback) =>
+					`.memo(function(${fnArg})${orig}onClickReply:(()=>{require('replyChain_handler').handle(${fnArg});${callback}()}),`
 			}
 		]
 	},
